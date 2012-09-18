e47bbc0eef0651b070c5fe8a98409e9894fe514d
Mitch Lloyd <mitch.lloyd@gmail.com>
2012-09-18 13:50:21 -0400

    Add artwork.

--- podcast_press_config.rb
+++ podcast_press_config.rb
@@ -1,7 +1,7 @@
+set clear: true
 set podcast_title: 'Gaslight Podcast'
 set artist: "Gaslight"
-set artwork: "./embedded-artwork.jpg"
-set clear: true
+set artwork: "./embedded_artwork.jpg"
 set date: Time.now
 set title: "Gaslight Podcast ##{get :episode_number}: #{get :title}"
 set filename: "gaslight-podcast-#{get :episode_number, min_digits: 3}.mp3"
